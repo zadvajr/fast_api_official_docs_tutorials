11c6ef8762657a1727721d313529cb0b17c7215e
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-31 21:03:09 +0100

    type hint helps with type checking too

--- python_type_intro.py
+++ python_type_intro.py
@@ -3,3 +3,17 @@
 #these type hints or annotations are special syntax that allow defining a type of variable
 #by declaring types for your variables editors and tools can give you better support.
 #fastapi is based on all these type hints - they give it many advantage or benefits
+#Simple Example
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+print(get_full_name("daniel", "zadva")) #outputs: Daniel Zadva
+#the point is type hints helps with autocompletion
+
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old " + str(age)
+    return name_with_age
+
+print(get_name_with_age("Daniel", 33))
+
