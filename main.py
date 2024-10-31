f2a11128f2f5b0bb166aa32d458f329626a1a756
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-31 23:35:55 +0100

    path parameters

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ async def root():
     return {
         "message": "Hello World!"
     }
+
+#Path parameters
+#You can delcare path parameter or variable with the same syntax used by python
+#strings
+@app.get("/items/{item_id}")
+async def get_item(item_id):
+    return { "item_id": item_id }
+#the value of the path parameter item_id will be passed to your function as argument item_id.
+#for example if you run this example and go to http://127.0.0.1:8000/items/foo you will get a response:
+# of { "item_id": "foo" }
+
+
+#Path parameters with type
