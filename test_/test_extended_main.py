13de6604ca5c8deadbde96788c8adbb6eeebdf80
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-11-22 01:43:29 +0100

    writing tests

---
+++ test_/test_extended_main.py
@@ -0,0 +1,59 @@
+from fastapi.testclient import TestClient
+from extended_main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/items/foo", headers={"X-Token": "10millioninthebank"})
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foo",
+        "title": "Foo",
+        "description": "There goes my Hero"
+    }
+
+def test_read_item_bad_token():
+    response = client.get("/items/foo", headers={"X-Token":"hailhydra"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+def test_read_nonexistent_item():
+    response = client.get("/items/baz", headers={"X-Token":"10millioninthebbank"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Item not found"}
+
+def test_create_item():
+    response = client.post(
+        "/items/",
+        headers={"X-Token": "coneofsilence"},
+        json={"id": "foobar", "title": "Foobar", "description": "Drop the bazz"}
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foobar",
+        "title": "Foo Bar",
+        "description": "The Foo Barters",
+    }
+def test_create_item_bad_token():
+    response = client.post(
+        "/items/",
+        headers={"X-Token": "halfmillion"},
+        json = {
+            "id": "bazz", "title": "Bazz", "description": "Drop the bazz"
+        }
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+def test_create_existing_item():
+    response = client.post(
+        "/items/",
+        headers={"X-Token", "coneofsilence"},
+        json={
+            "id": "foo",
+            "title": "The Foo ID Stealers",
+            "description": "There goes my stealers"
+        }
+    )
+    assert response.status_code == 409
+    assert response.json() == {"detail": "Item already exists"}
