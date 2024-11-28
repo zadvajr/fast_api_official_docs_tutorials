680c87f52db2fdc4c6648caee2adc7795885c065
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-11-29 00:43:18 +0100

    created endpoints to demonstrate request body

---
+++ request_body.py
@@ -0,0 +1,34 @@
+#Request body
+#When you need to send data from your client, let's say a browser to your API
+#you make use of request body.
+#Request body is a data sent to your API by the client.
+#Response body on the other hand is a data sent to client by your API
+
+#Your API almost always has to send a response body. But client don't necessarily 
+# need to send a request body all of the time
+#Sometime they only request a path with some query parameters but don't send a body.
+
+#To declare a request body you make use of pydantic models with all their power and benefits.
+#To send data you should make use of one of POST(the most common), PUT, DELETE, PATCH
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+item_db = []
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post('/items/')
+async def create_item(item: Item):
+    item_db.append(item)
+    return {"message": "Item added successfully"}
+
+@app.get('/items/')
+async def get_items():
+    return item_db
