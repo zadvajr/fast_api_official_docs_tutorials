ac2d70774ee18c4450acd22a9ab9aa79b938b056
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-11-29 01:54:37 +0100

    NA

--- tutorial_user_guide/request_body.py
+++ tutorial_user_guide/request_body.py
@@ -11,7 +11,7 @@
 #To declare a request body you make use of pydantic models with all their power and benefits.
 #To send data you should make use of one of POST(the most common), PUT, DELETE, PATCH
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from uuid import uuid1
 
@@ -34,3 +34,6 @@ async def create_item(item: Item):
 @app.get('/items/')
 async def get_items():
     return item_db
+
+def log_mid(request: Request, call_next):
+    pass
