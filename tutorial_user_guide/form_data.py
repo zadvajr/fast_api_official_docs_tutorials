b446be37350d5ca80d9a18612a9fac92906d3ab5
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-12-03 01:13:38 +0100

    form data

--- tutorial_user_guide/form_data.py
+++ tutorial_user_guide/form_data.py
@@ -1,3 +1,7 @@
+#when you need to receive form fields instead of json, you can make use of Form
+#To use Form you need to first install python-multipart
+#data from form is encoded as application/x-www-form-urlencoded
+#but when the form includes file, it is encoded as multipart/form-data
 from typing import Annotated
 from fastapi import FastAPI, Form
 
